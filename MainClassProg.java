d0c20fcaa4faa40e0ff775ad80cd2a4c7022dec4
Nandini Roy <nandini12majumdar@gmail.com>
2020-01-20 23:09:41 +0530

    Test Java Source COde

---
+++ MainClassProg.java
@@ -0,0 +1,10 @@
+public class MainClassProg
+{
+    public static void main(String[] args)
+    {
+        //int i = 10 + + 11 - - 12 + + 13 - - 14 + + 15;
+		//int i = 10 + + 11;
+ 
+        System.out.println(10 + + 11- - 12);
+    }
+}
